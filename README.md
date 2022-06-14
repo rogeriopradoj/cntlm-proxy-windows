15efc958699a03c242236717cbeb036430bce594
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2022-06-13 23:04:21 -0300

    Ajusta estrutura raiz projeto e pasta vendor

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 Rode, no Windows, um proxy HTTP de maneira fácil.
 
-Útil para acessar por linha de comando ferramentas de gestão de dependências (ex. composer, npm, nuget) com URLs que são acessíveis via navegador mas não funcionam via terminal (wget, curl).
+Útil para acessar por linha de comando ferramentas de gestão de dependências (ex. composer, npm, nuget, pip, conda, mamba) com URLs que são acessíveis via navegador mas não funcionam via terminal (wget, curl).
 
-Agora, em duas versões de proxy, (muito obrigado @la-costa por compartilhar sobre o px):
+Agora, em duas versões de proxy, (muito obrigado [@la-costa](https://github.com/la-costa) por compartilhar sobre o px):
 
 - [cntlm](http://cntlm.sourceforge.net/) (necessário digitar seu password no momento da execução)
 - [px](https://github.com/genotrance/px) (não é necessário fazer nenhuma configuração, pega os dados da sua sessão do Windows automaticamente)
@@ -20,17 +20,18 @@ de um arquivo de exemplo, use o `.env.example`
 
 ### Com o px (mais simples, recomendado)
 
-1. Execute o `px_start.bat` e siga as orientações em tela até aparecer a mensagem `Serving at :3128 proc MainProcess`;
+1. Execute o `start_px.bat` e siga as orientações em tela até aparecer a mensagem `Serving at :3128 proc MainProcess`;
 
-### Com o cntml (tem um passo a mais, digitar sua senha)
+### Com o cntlm (tem um passo a mais, digitar sua senha)
 
-1. Execute o `cntlm_start.bat` e siga as orientações em tela até colocar a senha;
+1. Execute o `start_cntlm.bat` e siga as orientações em tela até colocar a senha;
 
 ## Depois do proxy ligado (cntlm ou px), como utilizar no seu terminal
 
 1. Abra outro terminal (cmd ou bash):
     1. **cmd**: arraste o arquivo `proxy_set_cmd.bat` e execute
     2. **bash**: escreva `source`, coloque um espaço, arraste o arquivo `proxy_set_bash.sh`, e execute
+    1. **powershell**: arraste o arquivo `proxy_set_powershell.ps1` e execute
 2. Nesse mesmo terminal que está, execute os comandos que precisam do proxy
 
 ## Como interromper
@@ -38,4 +39,4 @@ de um arquivo de exemplo, use o `.env.example`
 Quando terminar, feche:
 
 - esse terminal que está trabalhando
-- a outra tela, que está rodando o cntlm_start.bat ou o px_start.bat
+- a outra tela, que está rodando o start_cntlm.bat ou o start_px.bat
---
+++ start_cntlm.bat
@@ -0,0 +1,106 @@
+@echo off
+title .==.                Inicializa cntlm              .==.
+rem Inicializa proxy cntlm
+
+mode con:cols=80 lines=100
+for /f "tokens=2 delims=:." %%x in ('chcp') do set cp=%%x
+chcp 1252>nul
+setlocal EnableDelayedExpansion
+
+set ENVFILE="%~dp0.env"
+IF NOT EXIST %ENVFILE% (
+    echo "Arquivo %ENVFILE% nao localizado. Dica: copie o arquivo .env.example da raiz do projeto"
+    goto:eof
+)
+
+call:GetEnvValue "CNTLM_NO_PROXY" CNTLM_NO_PROXY
+call:GetEnvValue "CNTLM_LISTEN" CNTLM_LISTEN
+call:GetEnvValue "CNTLM_PROXY" CNTLM_PROXY
+
+if not "%1" == "-y" (
+    goto prompt_user
+)
+goto main
+
+:prompt_user
+cls
+echo.
+echo.==================================================================
+echo.==                           CONFIGS                            ==
+echo.==================================================================
+echo.
+echo. Voce pode passar o parametro -y para nao ser questionado e
+echo. deixar os valores padroes serem utilizados.
+echo. 
+echo.     ^> proxy_start.bat -y
+echo. 
+echo. Observacao.: A senha(Password) sempre e solicitada.
+echo.
+echo.==================================================================
+echo.
+pause
+
+cls
+echo.
+echo.==================================================================
+echo.==                           CONFIGS                            ==
+echo.==================================================================
+echo.
+
+echo.   - CNTLM_NO_PROXY: !CNTLM_NO_PROXY!
+echo.
+set /p CNTLM_TEMP_VAR_1="...Digite novo valor, ou <ENTER> para continuar..."
+if not "!CNTLM_TEMP_VAR_1!" == "" (
+    set CNTLM_NO_PROXY="!CNTLM_TEMP_VAR_1!"
+    echo.   **Alterado** CNTLM_NO_PROXY: !CNTLM_NO_PROXY!
+)
+echo.
+
+echo.   - CNTLM_LISTEN: !CNTLM_LISTEN!
+echo.
+set /p CNTLM_TEMP_VAR_2="...Digite novo valor, ou <ENTER> para continuar..."
+if not "!CNTLM_TEMP_VAR_2!" == "" (
+    set CNTLM_LISTEN="!CNTLM_TEMP_VAR_2!"
+    echo.   **Alterado** CNTLM_LISTEN: !CNTLM_LISTEN!
+)
+echo.
+
+echo.   - CNTLM_PROXY: !CNTLM_PROXY!
+echo.
+set /p CNTLM_TEMP_VAR_3="...Digite novo valor, ou <ENTER> para continuar..."
+if not "!CNTLM_TEMP_VAR_3!" == "" (
+    set CNTLM_PROXY="!CNTLM_TEMP_VAR_3!"
+    echo.   **Alterado** CNTLM_PROXY: !CNTLM_PROXY!
+)
+echo.
+
+goto main
+
+:main
+cls
+echo.
+echo.==================================================================
+echo.==                           CONFIGS                            ==
+echo.==================================================================
+echo.
+echo.   - CNTLM_NO_PROXY: !CNTLM_NO_PROXY!
+echo.
+echo.   - CNTLM_LISTEN: !CNTLM_LISTEN!
+echo.
+echo.   - CNTLM_PROXY: !CNTLM_PROXY!
+echo.
+echo.   - USERDOMAIN: %USERDOMAIN%
+echo.
+echo.   - USERNAME:   %USERNAME%
+echo.
+echo.==================================================================
+echo.
+
+%~dp0vendor\cntlm-v0.92.2\cntlm.exe -v -d %userdomain% -u %username% -I -l !CNTLM_LISTEN! -N !CNTLM_NO_PROXY! !CNTLM_PROXY!
+
+endlocal
+chcp %cp%>nul
+
+:GetEnvValue
+@FOR /F "tokens=2 delims==" %%a in ('%WINDIR%\system32\find "%~1=" %ENVFILE%') do @set %~2=%%a
+@goto:eof
