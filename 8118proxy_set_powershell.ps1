1c788ee20eba0115097372e12d788941920db6b1
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2022-06-13 22:22:38 -0300

    Seta variáveis proxy na porta 8118 (e não 3128)

---
+++ 8118proxy_set_powershell.ps1
@@ -0,0 +1,22 @@
+Write-Host
+Write-Host "==> set http_proxy, https_proxy and all_proxy environment variables"
+$http_proxy = "http://127.0.0.1:8118"
+
+
+$Env:http_proxy = $http_proxy
+$Env:https_proxy = $http_proxy
+$Env:all_proxy = $http_proxy
+
+### Modify a user environment variable ###
+[Environment]::SetEnvironmentVariable("http_proxy", $http_proxy, [System.EnvironmentVariableTarget]::User)
+[Environment]::SetEnvironmentVariable("https_proxy", $http_proxy, [System.EnvironmentVariableTarget]::User)
+[Environment]::SetEnvironmentVariable("all_proxy", $http_proxy, [System.EnvironmentVariableTarget]::User)
+
+Write-Host
+Write-Host "   - http_proxy : " $Env:http_proxy
+Write-Host "   - https_proxy: " $Env:https_proxy
+Write-Host "   - all_proxy: "   $Env:all_proxy
+Write-Host
+Write-Host ".ok"
+
+Write-Host
